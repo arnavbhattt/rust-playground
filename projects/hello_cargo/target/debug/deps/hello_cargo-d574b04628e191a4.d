55a1c1297ee159d75eee4d443fcd65fe0a818230
arnavbhattt <arnxv23@gmail.com>
2025-09-14 02:09:05 -0400

    hello_world w cargo

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-d574b04628e191a4.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/arnav/rust-playground/projects/hello_cargo/target/debug/deps/hello_cargo-d574b04628e191a4.d: src/main.rs
+
+/mnt/c/Users/arnav/rust-playground/projects/hello_cargo/target/debug/deps/hello_cargo-d574b04628e191a4: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/arnav/rust-playground/projects/hello_cargo/target/debug/hello_cargo: /mnt/c/Users/arnav/rust-playground/projects/hello_cargo/src/main.rs
