0b5940c0e4dd0c1a3ab85f705b9c62c09189cf6a
arnavbhattt <arnxv23@gmail.com>
2025-09-14 02:18:03 -0400

    variable types, mutability, etc

---
+++ projects/guessing_game/target/debug/deps/guessing_game-d2d0629a6acb61e3.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/arnav/rust-playground/projects/guessing_game/target/debug/deps/guessing_game-d2d0629a6acb61e3.d: src/main.rs
+
+/mnt/c/Users/arnav/rust-playground/projects/guessing_game/target/debug/deps/guessing_game-d2d0629a6acb61e3: src/main.rs
+
+src/main.rs:
---
+++ projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/arnav/rust-playground/projects/guessing_game/target/debug/guessing_game: /mnt/c/Users/arnav/rust-playground/projects/guessing_game/src/main.rs
